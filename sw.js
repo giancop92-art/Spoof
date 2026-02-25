762c32a369703551dcb3c5ba3b6a90de2431acaf
giancop92-art <giancop92@gmail.com>
2026-02-26 00:22:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+ './spoon.png',     // Aggiunto
+  './teaspoon.png' // Aggiunto
 ];
 
 // Installazione: salva i file nella memoria del telefono
