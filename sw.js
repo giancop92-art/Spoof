8ba512e99a389f8395543cfbeb59f96bb4e647cb
giancop92-art <giancop92@gmail.com>
2026-02-27 18:52:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
 /* sw.js - Chef Academy / Peso Senza Bilancia */
-
-// Cambia questo numero (v1, v2, v3...) ogni volta che modifichi index.html
-const CACHE_NAME = 'chef-academy-v4'; 
-
+const CACHE_NAME = 'chef-academy-v5'; 
 const ASSETS = [
   './',
   './index.html',
@@ -21,30 +18,17 @@ const ASSETS = [
   './milk.png'
 ];
 
-// Installazione: salvataggio asset
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// Attivazione: pulizia vecchie cache
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => {
-        if (k !== CACHE_NAME) return caches.delete(k);
-      })
-    ))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
   self.clients.claim();
 });
 
-// Fetch: Priorità alla cache per velocità massima
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
